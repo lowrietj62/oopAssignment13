8ea818c88e92684879f9f7977ce7f8d718d684d7
lowrietj62 <lowrietj62@gmail.com>
2026-04-09 13:48:51 -0400

    Added Header for Rental_Shop

--- classes/rental_shop.py
+++ classes/rental_shop.py
@@ -1,3 +1,10 @@
+from inventory import Inventory
+
+#--------------------------------------------------------------------------
+# Class Name: Rental_Shop
+# Description: The main class for handling anything to do with the ski rental shop
+#--------------------------------------------------------------------------
+
 class Rental_Shop:
     
     def __init__(self):
