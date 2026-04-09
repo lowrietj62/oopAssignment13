6da7f3b4765872e5a51e5b8e75bf82baf52f604d
patel <darshankpatel2005@gmail.com>
2026-04-09 13:46:02 -0400

    Changes

--- classes/rental.py
+++ classes/rental.py
@@ -1,4 +1,13 @@
 class Rental:
     
-    def __init__(self):
-        pass
+    def __init__(self,item_type,quantity,rental_type):
+        self._item_type = item_type
+        self._quantity = quantity
+        self._rental_type = rental_type  # hourly, daily, weekly
+        
+    def get_detail(self):
+        return{ 
+            "item" : self._item_type,
+            "qty" : self._quantity,
+            "type" : self._rental_type,
+        }
