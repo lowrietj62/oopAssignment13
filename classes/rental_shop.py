1cecfbb1771eacfcc39e4949db3a5d34ad23b2da
lowrietj62 <lowrietj62@gmail.com>
2026-04-10 15:24:42 -0400

    Fixed comparison in process_rental

--- classes/rental_shop.py
+++ classes/rental_shop.py
@@ -7,6 +7,7 @@ from inventory import Inventory
 
 class Rental_Shop:
     
+    #Initialization
     def __init__(self, skis = 0, snowboards = 0):
         
         # Encapsulation: Inventory object is stored internally
@@ -31,17 +32,24 @@ class Rental_Shop:
         quantity = rental.getQuantity()
 
         # Check availability
-        if not self._inventory.check_availability(equipment_type, quantity):
+        available  = self._inventory.check_availability(equipment_type, quantity)
+
+        if available == True:
+            
+            # Update inventory
+            self._inventory.update_stock(equipment_type, quantity, rent = True)
+
+            # Add rental to active list
+            self._activeRentals.appent(rental)
+
+            print("Rental processed succesfully")
+            return rental      
+         
+        else:
             print("Requested quantity not available.")
             return None
-        
-        # Update inventory
-        self._inventory.update_stock(equipment_type, quantity, rent = True)
-
-        # Add rental to active list
-        self._activeRentals.appent(rental)
-
-        print("Rental processed succesfully")
-        return rental
     
-    
+    # Return Rental (Triggers Billing)
+    def return_rental(self, rental):
+
+        if rental 
