579fcb9f3192541b0be7de6927f7596e28ef1880
lowrietj62 <lowrietj62@gmail.com>
2026-04-10 15:11:15 -0400

    Finished init and display_inventory for rental_shop

--- classes/rental_shop.py
+++ classes/rental_shop.py
@@ -7,5 +7,21 @@ from inventory import Inventory
 
 class Rental_Shop:
     
-    def __init__(self):
-        pass
+    def __init__(self, skis = 0, snowboards = 0):
+        
+        # Encapsulation: Inventory object is stored internally
+        self._inventory = Inventory(skis, snowboards)
+
+        # List to track active rentals
+        self._activeRentals = []
+
+    # Display Inventory
+    def display_inventory(self): 
+
+        skis, snowboards = self._inventory.get_stock()
+
+        print("Current Inventory:")
+        print("Skis Available", skis)
+        print("Snowboards Available", snowboards)
+
+
